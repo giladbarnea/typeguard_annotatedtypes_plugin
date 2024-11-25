26271fa20c8a41a0aacddddfa334013873afff71
Gilad Barnea <giladbrn@gmail.com>
2024-11-25 17:02:05 +0200

    add support for all VALIDATORS in at test cases

--- src/typeguard_annotatedtypes_plugin/__init__.py
+++ src/typeguard_annotatedtypes_plugin/__init__.py
@@ -1,6 +1,8 @@
+from datetime import datetime, timezone
 from functools import partial
 from typing import Any, Optional
 
+import annotated_types
 from annotated_types import BaseMetadata, Predicate
 from typeguard import (
     TypeCheckError,
@@ -8,6 +10,75 @@ from typeguard import (
     check_type,
     checker_lookup_functions,
 )
+
+
+def check_gt(constraint, val) -> bool:
+    assert isinstance(constraint, annotated_types.Gt)
+    return val > constraint.gt
+
+
+def check_lt(constraint, val) -> bool:
+    assert isinstance(constraint, annotated_types.Lt)
+    return val < constraint.lt
+
+
+def check_ge(constraint, val) -> bool:
+    assert isinstance(constraint, annotated_types.Ge)
+    return val >= constraint.ge
+
+
+def check_le(constraint, val) -> bool:
+    assert isinstance(constraint, annotated_types.Le)
+    return val <= constraint.le
+
+
+def check_multiple_of(constraint, val) -> bool:
+    assert isinstance(constraint, annotated_types.MultipleOf)
+    return val % constraint.multiple_of == 0
+
+
+def check_len(constraint, val) -> bool:
+    if isinstance(constraint, slice):
+        constraint = annotated_types.Len(constraint.start or 0, constraint.stop)
+    assert isinstance(constraint, annotated_types.Len)
+    if constraint.min_inclusive is None:
+        raise TypeError
+    if len(val) < constraint.min_inclusive:
+        return False
+    if constraint.max_exclusive is not None and len(val) >= constraint.max_exclusive:
+        return False
+    return True
+
+
+def check_predicate(constraint, val) -> bool:
+    assert isinstance(constraint, annotated_types.Predicate)
+    return constraint.func(val)
+
+
+def check_timezone(constraint, val) -> bool:
+    assert isinstance(constraint, annotated_types.Timezone)
+    assert isinstance(val, datetime)
+    if isinstance(constraint.tz, str):
+        return val.tzinfo is not None and constraint.tz == val.tzname()
+    elif isinstance(constraint.tz, timezone):
+        return val.tzinfo is not None and val.tzinfo == constraint.tz
+    elif constraint.tz is None:
+        return val.tzinfo is None
+    # ellipsis
+    return val.tzinfo is not None
+
+
+VALIDATORS = {
+    annotated_types.Gt: check_gt,
+    annotated_types.Lt: check_lt,
+    annotated_types.Ge: check_ge,
+    annotated_types.Le: check_le,
+    annotated_types.MultipleOf: check_multiple_of,
+    annotated_types.Predicate: check_predicate,
+    annotated_types.Len: check_len,
+    annotated_types.Timezone: check_timezone,
+    slice: check_len,
+}
 
 
 def check_predicate(
@@ -66,7 +137,7 @@ def check_annotated_type(
         collection_check_strategy=memo.config.collection_check_strategy,
     )
     try:
-        check_ok = value >= annotated_type.ge
+        check_ok = VALIDATORS[type(annotated_type)](annotated_type, value)
     except Exception as e:
         raise TypeCheckError(f"with {value=!r} raised an error: {e!r}") from None
     else:
